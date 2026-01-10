d6da9866bfc9fc35d3ac992da62986336d754e47
Gabriel Henrique Pascon <gh.pascon@gmail.com>
2026-01-10 13:22:13 -0300

    op

--- src/publish_lib_ghp/__init__.py
+++ src/publish_lib_ghp/__init__.py
@@ -2,10 +2,7 @@
 
 import sys
 
-if sys.version_info >= (3, 8):
-    from importlib.metadata import version
-else:
-    from importlib_metadata import version
+from importlib.metadata import version
 
 try:
     __version__ = version("publish-lib-ghp")
@@ -13,9 +10,5 @@ except Exception:
     # Fallback for development environments
     __version__ = "0.0.0-dev"
 
-__author__ = "Gabriel Henrique Pascon"
-__email__ = "gh.pascon@gmail.com"
-
 from .greeting import Greeting
-
-__all__ = ["Greeting", "__version__"]
+from .operations import Operations
